86b7782a2ead71783ceac2c150525b486d0942d3
Benjamin Ross <benjaminross1001@gmail.com>
2017-05-30 17:38:54 -0600

    second feature test

--- spec/features/test_03_browsing_items_by_category_spec.rb
+++ spec/features/test_03_browsing_items_by_category_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "visitor can browse items by category" do
-  scenario "they can see the page with "
+  scenario "they can see the page for a specific category" do
+    category1 = create(:category)
+    item1 = category1.items.first ###?????
+    item2 = category1.items.last  ###????
+
+    visit category_path(category1)
+
+    expect(page).to have_content(item1.title)
+    expect(page).to have_content(item2.title)
+  end
 end
