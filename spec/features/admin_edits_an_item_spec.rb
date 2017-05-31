746487d7420a9c864d32c5d325c0ff8a1279605a
NataliaColome <nmcolome@gmail.com>
2017-05-31 17:10:34 -0600

    refactor and correct name

--- spec/features/admin_edits_an_item_spec.rb
+++ spec/features/admin_edits_an_item_spec.rb
@@ -3,21 +3,14 @@ require 'rails_helper'
 RSpec.describe "user edits an item" do
   context "as admin" do
     it "they can edit an item" do
-      admin = User.create(email: "adumbledore@email.com"
-                        username: "adumbledore",
-                        password: "password",
-                        role: 1)
-      category = Category.create(name: "Yoga")
+      admin = create(:user, role: 1)
       item1 = create(:item)
 
-      visit login_path
-      fill_in "Username", with: admin.username
-      fill_in "Password", with: "password"
-      click_button "Sign In"
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
       visit "/admin/dashboard"
       click_on "View All Items"
-      click_on "Edit"
+      click_on "Edit Item"
 
       expect(current_path).to eq(edit_admin_items_path(item1)) #"/admin/items/:ITEM_ID/edit"
       fill_in "Description", with "new description"
@@ -30,21 +23,13 @@ RSpec.describe "user edits an item" do
 
   context "as registered user" do
     it "they can't see the page" do
-      user = User.create(email: "hpotter@email.com"
-                        username: "hpotter",
-                        password: "password",
-                        role: 0)
-      category = Category.create(name: "Yoga")
+      user = create(:user, role: 1)
       item1 = create(:item)
-
-      visit login_path
-      fill_in "Username", with: user.username
-      fill_in "Password", with: "password"
-      click_button "Sign In"
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
       visit edit_admin_items_path(item1)
 
-      expect(page).to_not have_content("Edit")
+      expect(page).to_not have_content("Edit Item")
       expect(page).to_not have_content("Submit")
       expect(page).to have_content("The page you were looking for doesn't exist.")
     end
@@ -52,7 +37,6 @@ RSpec.describe "user edits an item" do
 
   context "as visitor" do
     it "they can't see the page" do
-      category = Category.create(name: "Yoga")
       item1 = create(:item)
 
       visit edit_admin_items_path(item1)
--- spec/features/admin_viewing_items_spec.rb
+++ spec/features/admin_viewing_items_spec.rb
@@ -3,18 +3,11 @@ require 'rails_helper'
 RSpec.describe "items view" do
   context "as admin" do
     it "they see all items" do
-      admin = User.create(email: "adumbledore@email.com"
-                        username: "adumbledore",
-                        password: "password",
-                        role: 1)
-      category = Category.create(name: "Yoga")
+      admin = create(:user, role: 1)
       item1, item2 = create_list(:item, 2)
       item3 = create(:item, status: 1)
 
-      visit login_path
-      fill_in "Username", with: admin.username
-      fill_in "Password", with: "password"
-      click_button "Sign In"
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
       visit "/admin/dashboard"
       click_on "View All Items"
@@ -28,35 +21,6 @@ RSpec.describe "items view" do
       expect(page).to have_content(item2.status)
       expect(page).to have_content(item2.price)
       expect(page).to have_link('Edit Item')
-    end
-  end
-
-  context "as registered user" do
-    it "they can't see the page" do
-      user = User.create(email: "hpotter@email.com"
-                        username: "hpotter",
-                        password: "password",
-                        role: 0)
-
-      visit login_path
-      fill_in "Username", with: user.username
-      fill_in "Password", with: "password"
-      click_button "Sign In"
-
-      visit "/admin/dashboard"
-
-      expect(page).to_not have_content("View All Items")
-      expect(page).to have_content("The page you were looking for doesn't exist.")
-    end
-  end
-
-  context "as visitor" do
-    it "they can't see the page" do
-
-      visit "/admin/dashboard"
-
-      expect(page).to_not have_content("View All Items")
-      expect(page).to have_content("The page you were looking for doesn't exist.")
     end
   end
 end
