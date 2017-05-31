ca01f2635c4e0332b4ee2f8e0c564a4fff5e9ab0
William Thomas <wthoma390@gmail.com>
2017-05-30 18:41:43 -0600

    write test for 08 so authenticated user can log in and update test 13 to double quotes

--- spec/features/test_08_authenticated_user_spec.rb
+++ spec/features/test_08_authenticated_user_spec.rb
@@ -1,1 +1,36 @@
 require 'rails_helper'
+
+RSpec.describe "Authenticated user can log in" do
+  scenario "user can log in and see profile" do
+    user = create(:user)
+    visit root_path
+
+    click_link "Login"
+
+    visit login_path
+
+    fill_in "Username", with: user.username
+    fill_in "Password", with: user.password
+    click_button "Login"
+
+    within '.navbar' do
+      expect(page).to have_content("Logged in as #{user.username}")
+    end
+    expect(current_path).to eq('/dashboard')
+    expect(page).to have_content("Hello, #{user.name}")
+    expect(page).to have_content("Logout")
+    expect(page).to_not have_content("Login")
+  end
+
+  scenario "user can not use bad credentials to log in" do
+    user = create(:user)
+
+    visit login_path
+
+    fill_in "Username", with: "ThisIsNotMyName"
+    fill_in "Password", with: user.password
+    click_button "Login"
+
+    expect(page).to have_content("Login to your account.")
+  end
+end
