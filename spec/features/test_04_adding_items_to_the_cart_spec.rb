9a4775c82487f0fbda9972309064a9a26f9fe195
Benjamin Ross <benjaminross1001@gmail.com>
2017-05-31 13:54:45 -0600

    after group test review

--- spec/features/test_04_adding_items_to_the_cart_spec.rb
+++ spec/features/test_04_adding_items_to_the_cart_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe "visitor can add items and remove from the cart" do
   scenario "they can click a link to add item to cart" do
     category = create(:category, name: "Surf")
     item1 = create(:item, title: "Wetsuit", category: category)
+    category2 = create(:category, name: "Surf")
+    item2 = create(:item, title: "Wetsuit", category: category2)
 
     visit category_path(category)
 
@@ -12,14 +14,23 @@ RSpec.describe "visitor can add items and remove from the cart" do
 
     click_on("Add to Cart")
 
+    visit category_path(category2)
+
+    click_on("Add to Cart")
+
+    expect(current_path).to eq(category_path(category2))
+
     click_on("View Cart")
 
     expect(current_path).to eq("/cart")
-    expect(page).to have_css('img')
+    expect(page).to have_css("img[src=\"#{item1.image}\"]")
     expect(page).to have_content(item1.title)
     expect(page).to have_content(item1.description)
     expect(page).to have_content(item1.price)
-    expect(page).to have_content("Total: #{item.price}")
+    expect(page).to have_content(item2.title)
+    expect(page).to have_content(item2.description)
+    expect(page).to have_content(item2.price)
+    expect(page).to have_content("Total: #{item1.price + item2.price}")
   end
 
   scenario "visitor starts with an item and removes" do
@@ -40,10 +51,10 @@ RSpec.describe "visitor can add items and remove from the cart" do
     click_on("Remove")
 
     expect(current_path).to eq("/cart")
-    expect(page).to have_css("green") ####????
+    expect(flash[:notice]).to have_css(".add_to_cart") # Double check
     expect(page).to have_content("Successfully removed #{item1.title} from your cart.")
     expect(page).to have_link(item_path(item1))
-    #(Test that the item is not in the cart)????? How do we test this is the content from line 44 is static? Won't it remain making it impossible for us to test expect(page).to_not have_content(item1.title)
+    expect(page).to_not have_css('h3', item1.title)
 
   end
 end
--- spec/features/test_06_adjusting_the_quantity_of_an_item_in_the_cart_spec.rb
+++ spec/features/test_06_adjusting_the_quantity_of_an_item_in_the_cart_spec.rb
@@ -4,34 +4,26 @@ RSpec.describe "visitor can visit the cart" do
   scenario "they can increase the quantity of an item" do
     category = create(:category, name: "Surf")
     item1 = create(:item, title: "Wetsuit", category: category)
-    item2 = create(:item, title: "Cold Weather Wax", category: category)
 
     visit category_path(category)
-
-    expect(page).to have_content(item1.title)
-    expect(page).to have_link("Add to Cart")
 
     click_on("Add to Cart")
 
     click_on("View Cart")
     expect(current_path).to eq("/cart")
     expect(page).to have_content("Quantity: 1") ## Look at amazon, they don't start with a set quantity, user selects from a dropdown then adds that number of items when they click on Add to Cart
-    select "2", :from => "Qty:"  ##Is this how we want our increase Qty dropdown to work??
-    expect(current_path).to eq("/cart")  ## By Selecting a different quantity, it shouldn't cause a page refresh
+    select "2", :from => "Qty:"  ## Dropdown or Text Field ??
+    expect(current_path).to eq("/cart")  # By Selecting a different quantity, it shouldn't cause a page refresh
     expect(page).to have_content("Quantity: 2")
     expect(page).to have_content("Subtotal: #{item1.price * 2}")
     expect(page).to have_content("Total: #{item1.price * 2}")
   end
 
-  scenario "they can increase the quantity of an item" do
+  scenario "they can decrease the quantity of an item" do
     category = create(:category, name: "Surf")
     item1 = create(:item, title: "Wetsuit", category: category)
-    item2 = create(:item, title: "Cold Weather Wax", category: category)
 
     visit category_path(category)
-
-    expect(page).to have_content(item1.title)
-    expect(page).to have_link("Add to Cart")
 
     click_on("Add to Cart")  ### Is this how we want to select 2 items
     click_on("Add to Cart")
