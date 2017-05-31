67930d4454234de29af6e77a2966d402537cba1c
squeemishly <squeemishly@gmail.com>
2017-05-31 16:21:29 -0600

    cleaned up admin test to reference '/admin/dashboard' instead of admin_path(admin)

--- spec/features/t_19_admin_logs_in_spec.rb
+++ spec/features/t_19_admin_logs_in_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe "As an admin" do
   it "when I log in, I can redirected to '/admin/dashboard'" do
     admin = create(:user, role: 1)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
     visit root_path
 
@@ -12,7 +11,7 @@ RSpec.describe "As an admin" do
     fill_in "Password", with: admin.password
     click_button "Login"
 
-    expect(current_path).to eq admin_path(admin)
+    expect(current_path).to eq '/admin/dashboard'
     expect(page).to have_content "Admin Dashboard"
   end
 end
