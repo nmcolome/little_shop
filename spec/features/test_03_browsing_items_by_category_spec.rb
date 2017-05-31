2186c6133cb40ff9067c085d5f4a6ceeff391266
Benjamin Ross <benjaminross1001@gmail.com>
2017-05-31 13:54:45 -0600

    after group test review

--- spec/features/test_03_browsing_items_by_category_spec.rb
+++ spec/features/test_03_browsing_items_by_category_spec.rb
@@ -8,7 +8,12 @@ RSpec.describe "visitor can browse items by category" do
 
     visit category_path(category)
 
+
+    expect(page).to have_content(category.name)
     expect(page).to have_content(item1.title)
     expect(page).to have_content(item2.title)
+    expect(page).to have_content(item1.price)
+    expect(page).to have_content(item2.price)
+    expect(page).to have_link("Add to Cart")
   end
 end
