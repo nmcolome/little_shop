c3f550062a0ab71e565f768c1c0e51f188082c55
William Thomas <wthoma390@gmail.com>
2017-05-31 14:38:51 -0600

    delete order_2 and update test

--- spec/features/test_13_viewing_a_past_order_spec.rb
+++ spec/features/test_13_viewing_a_past_order_spec.rb
@@ -3,11 +3,10 @@ require 'rails_helper'
 RSpec.describe "User views a single order" do
   scenario "user can view an order that belongs to them"do
     user = create(:user_with_orders)
-      order = user.orders.first
+      order_1 = user.orders.first
       item_1 = create(:item)
       item_2 = create(:item)
-      OrderItem.create!(order_id: order_1.id, item_id: item_1.id)
-      OrderItem.create(order_id: order_1.id, item_id: item_2.id)
+      OrderItem.create(order_id: order_1.id, item_id: item_1.id)
 
       visit login_path
 
@@ -15,19 +14,17 @@ RSpec.describe "User views a single order" do
       fill_in "Password", with: user.password
       click_button "Login"
 
-      visit orders_path
+      visit user_orders_path(user)
 
-      expect(page).to have_link("View order", href: order_path(order_1))
+      expect(page).to have_link("Order #{order_1.id}", href: user_order_path(user, order_1))
 
-      click_link "View order", href: order_path(order)
+      click_link "Order #{order_1.id}", href: user_order_path(user, order_1)
 
-      expect(page).to have_content("#{item_1.title} $#{item_1.price}")
-      expect(page).to have_content("#{item_2.title} $#{item_2.price}")
+#What should we write for old order specific item quantity???
+# ActiveRecord or price in table for order_1.price???
+      expect(page).to have_content("#{item_1.title} $#{item_1.price} $#{order.order_items.first.quantity}")
       expect(page).to have_link(item_1.title, href: item_path(item_1))
-      expect(page).to have_link(item_2.title, href: item_path(item_2))
       expect(page).to have_content(order_1.status)
-      expect(page).to have_content(order_2.status)
       expect(page).to have_content(order_1.price)
-      expect(page).to have_content(order_2.price)
   end
 end
