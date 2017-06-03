a71547b072d520ae170da558136d1e8beb6d5761
NataliaColome <nmcolome@gmail.com>
2017-06-03 14:29:19 -0600

    create condition to display 'retired item' or 'add to cart'

--- spec/features/user/retired_items_spec.rb
+++ spec/features/user/retired_items_spec.rb
@@ -1,19 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "when a user views a retired item" do
-  # it "should say 'Item Retired'" do
-  #   user = create(:user)
-  #   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-  #
-  #   item = create(:item, status: "retired")
-  #
-  #   visit item_path(item)
-  #
-  #   expect(page).to have_content item.title
-  #   expect(page).to have_content item.description
-  #   expect(page).to have_content item.image
-  #
-  #   expect(page).to_not have_content "Add to Cart"
-  #   expect(page).to have_content "Item Retired"
-  # end
+  it "should say 'Item Retired'" do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    item = create(:item, status: "retired")
+
+    visit item_path(item)
+
+    expect(page).to have_content item.title
+    expect(page).to have_content item.description
+    # expect(page).to have_content item.image
+  
+    expect(page).to_not have_content "Add to Cart"
+    expect(page).to have_content "Item Retired"
+  end
 end
