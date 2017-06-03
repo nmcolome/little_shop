c7291f7f02f4ec8882cc6b92067f0d5fd6af01f6
Benjamin Ross <benjaminross1001@gmail.com>
2017-06-03 15:18:25 -0600

    creates user authentication through url with before action in the user controller

--- spec/features/user/authenticated_users_security_spec.rb
+++ spec/features/user/authenticated_users_security_spec.rb
@@ -1,38 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe "An authenticated regular user" do
-  # before(:each) do
-  #   user = create(:user)
-  #   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-  # end
-  #
-  # it "cannot see another user's private data" do
-  #   user2 = create(:user)
-  #   visit login_path
-  #
-  #   fill_in "Username", with: user2.username
-  #   fill_in "Password", with: user2.password
-  #   click_button "Login"
-  #
-  #   visit user_path(user)
-  #   expect(page).to have_content "The page you were looking for doesn't exist."
-  # end
-  #
-  # it "cannot view the administrator screens or use admin functionality" do
-  #   visit '/admin/dashboard'
-  #   item = create(:item)
-  #
-  #   expect(page).to_not have_content "Admin Dashboard"
-  #   expect(page).to have_content "The page you were looking for doesn't exist."
-  #
-  #   visit admin_item_path(item)
-  #   expect(page).to have_content "The page you were looking for doesn't exist."
-  #
-  #   visit new_admin_item_path
-  #   expect(page).to have_content "The page you were looking for doesn't exist."
-  # end
-  #
-  # it "user cannot make themselves an admin" do
-  #
-  # end
+  before(:each) do
+    # allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+  end
+
+  it "cannot see another user's private data" do
+    user = create(:user, first_name: "Natalia")
+    user2 = create(:user, first_name: "Mogley")
+    visit login_path
+
+    fill_in "Username", with: user2.username
+    fill_in "Password", with: user2.password
+    click_button "Login"
+
+    #visit dashboard_path(user)
+    visit edit_user_path(user)
+
+    expect(page).to_not have_content user.first_name
+    expect(page).to have_content "The page you were looking for doesn't exist."
+  end
+
+  xit "cannot view the administrator screens or use admin functionality" do
+    visit '/admin/dashboard'
+    item = create(:item)
+
+    expect(page).to_not have_content "Admin Dashboard"
+    expect(page).to have_content "The page you were looking for doesn't exist."
+
+    visit admin_item_path(item)
+    expect(page).to have_content "The page you were looking for doesn't exist."
+
+    visit new_admin_item_path
+    expect(page).to have_content "The page you were looking for doesn't exist."
+  end
+
+  it "user cannot make themselves an admin" do
+
+  end
 end
