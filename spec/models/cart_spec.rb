f2434d566fd0a7d9454dc96d02c4c8c742d35499
squeemishly <squeemishly@gmail.com>
2017-06-04 14:35:35 -0600

    tests cart model for total, subtotal, increase quantity, & decrease quantity

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,37 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Cart do
-  # describe "#total" do
-  #   it "can calculate the total number of items it holds" do
-  #     item = Item.create(title: "itemz", description: "greatest", price: 4, status: 0)
-  #     item_2 = Item.create(title: "scuba", description: "worst", price: 3, status: 0)
-  #
-  #     @cart = Cart.new({item.id.to_s => 2, item_2.id.to_s => 3})
-  #     expect(@cart.total).to eq(17)
-  #   end
-  # end
-  #
-  # describe "#find_quantity_subtotal" do
-  #   it "finds quantity and subtotal" do
-  #     item = Item.create(title: "itemz", description: "greatest", price: 4, status: 0)
-  #     item_2 = Item.create(title: "scuba", description: "worst", price: 3, status: 0)
-  #
-  #     @cart = Cart.new({item.id.to_s => 2, item_2.id.to_s => 3})
-  #
-  #     @cart.find_quantity_subtotal
-  #
-  #     expect(@cart.contents.values).to eq([2, 3])
-  #   end
-  # end
-  #
-  # describe "#update_quantity" do
-  #   it "updates content quantity" do
-  #     item = Item.create(title: "itemz", description: "greatest", price: 4, status: 0)
-  #     item_2 = Item.create(title: "scuba", description: "worst", price: 3, status: 0)
-  #
-  #     @cart = Cart.new({item.id.to_s => 2, item_2.id.to_s => 3})
-  #     expect(@cart.update_quantity(item.id)).to eq(3)
-  #     expect(@cart.update_quantity(item_2.id)).to eq(4)
-  #   end
-  # end
+  describe "#total" do
+    it "can calculate the total number of items it holds" do
+      item = Item.create(title: "itemz", description: "greatest", price: 4, status: 0)
+      item_2 = Item.create(title: "scuba", description: "worst", price: 3, status: 0)
+
+      cart = Cart.new({item.id.to_s => 2, item_2.id.to_s => 3})
+      expect(cart.total).to eq(17)
+    end
+  end
+
+  describe "#find_quantity_subtotal" do
+    it "finds quantity and subtotal" do
+      item = Item.create(title: "itemz", description: "greatest", price: 4, status: 0)
+      item_2 = Item.create(title: "scuba", description: "worst", price: 3, status: 0)
+
+      cart = Cart.new({item.id.to_s => 2, item_2.id.to_s => 3})
+
+      cart.find_quantity_subtotal
+
+      expect(cart.contents.values).to eq([2, 3])
+    end
+  end
+
+  describe "#increase_quantity" do
+    it "increases content quantity" do
+      item = Item.create(title: "itemz", description: "greatest", price: 4, status: 0)
+      item_2 = Item.create(title: "scuba", description: "worst", price: 3, status: 0)
+
+      cart = Cart.new({item.id.to_s => 2, item_2.id.to_s => 3})
+      expect(cart.increase_quantity(item.id)).to eq(3)
+      expect(cart.total).to eq 21
+    end
+  end
+
+  describe "#decrease_quantity" do
+    it "decreases content quantity" do
+      item = Item.create(title: "itemz", description: "greatest", price: 4, status: 0)
+      item_2 = Item.create(title: "scuba", description: "worst", price: 3, status: 0)
+
+      cart = Cart.new({item.id.to_s => 2, item_2.id.to_s => 3})
+      expect(cart.decrease_quantity(item.id)).to eq(1)
+      expect(cart.total).to eq 13
+    end
+  end
 end
