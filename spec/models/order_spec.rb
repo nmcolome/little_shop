a08d8686c148d016623751539aea83fe405e005a
squeemishly <squeemishly@gmail.com>
2017-06-04 17:34:12 -0600

    order now tests relationship to items

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -11,4 +11,15 @@ RSpec.describe Order do
 
     expect(order1.total).to eq 11
   end
+
+  it "has access to items" do
+    item = create(:item, price: 3)
+    item2 = create(:item, price: 4)
+    user = create(:user)
+    order1 = create(:order, user_id: user.id)
+    OrderItem.create(item_id: item.id, order_id: order1.id, quantity: 1)
+    OrderItem.create(item_id: item2.id, order_id: order1.id, quantity: 2)
+
+    expect(order1).to respond_to(:items)
+  end
 end
