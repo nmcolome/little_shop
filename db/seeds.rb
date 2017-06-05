3594d82a81e0af7b70197b5948bf835d96815e1e
squeemishly <squeemishly@gmail.com>
2017-06-05 11:51:23 -0600

    seed file has users and admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do
+  user = User.create(first_name: Faker::Name.first_name,
+              last_name: Faker::Name.last_name,
+              username: Faker::Cat.unique.name + ((1..1000).to_a).sample.to_s,
+              email: Faker::Internet.unique.email  + ((1..1000).to_a).sample.to_s,
+              password: "password",
+              address: Faker::Address.street_address,
+              city: Faker::Address.city,
+              state: Faker::Address.state,
+              zipcode: Faker::Address.zip_code)
+  puts "You created #{user.username} (who is actually #{user.first_name} #{user.last_name}, email: #{user.email})"
+end
+
+admin = User.create(first_name: "admin",
+            last_name: "mcadminy",
+            username: "admin",
+            email: "admin@mcadminy.com",
+            password: "password",
+            address: Faker::Address.street_address,
+            city: Faker::Address.city,
+            state: Faker::Address.state,
+            zipcode: Faker::Address.zip_code,
+            role: 1)
+puts "You created an admin!"
--- spec/features/user/user_can_log_out_spec.rb
+++ spec/features/user/user_can_log_out_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe "Logged in user can log out" do
   scenario "user sees the visitor options page" do
     user = create(:user)
-  
+
     visit root_path
-  
+
     click_link "Login"
     fill_in "Username", with: user.username
     fill_in "Password", with: user.password
