5d697096749fbf1c0877c036f96da2bc715617b3
William Thomas <wthoma390@gmail.com>
2017-06-06 13:35:11 -0600

    Paperclip model, migration, gem and view

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
+  has_attached_file :image_att, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image_att, content_type: /\Aimage\/.*\z/
+
   validates :title, presence: true
   validates :description, presence: true
   validates :price, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170601165413) do
+ActiveRecord::Schema.define(version: 20170606192813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,12 +32,16 @@ ActiveRecord::Schema.define(version: 20170601165413) do
   end
 
   create_table "items", force: :cascade do |t|
-    t.string   "title",                   null: false
-    t.string   "description",             null: false
-    t.decimal  "price",                   null: false
-    t.integer  "status",      default: 0
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.string   "title",                              null: false
+    t.string   "description",                        null: false
+    t.decimal  "price",                              null: false
+    t.integer  "status",                 default: 0
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+    t.string   "image_att_file_name"
+    t.string   "image_att_content_type"
+    t.integer  "image_att_file_size"
+    t.datetime "image_att_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
