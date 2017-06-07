82f8f1a4bcddf5d8ccaf8f25d288d44d4fc66c2b
squeemishly <squeemishly@gmail.com>
2017-06-07 16:12:10 -0600

    resolved merge conflict

--- spec/features/admin/admin_can_filter_orders_by_status_spec.rb
+++ spec/features/admin/admin_can_filter_orders_by_status_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "an admin can filter orders list by status" do
-  scenario "when they are in their dashboard" do
+  scenario "to only see ordered items" do
     admin = create(:user, role: 1)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
     order1 = create(:order_with_items, status: 0)
@@ -16,19 +16,87 @@ RSpec.describe "an admin can filter orders list by status" do
     expect(page).to have_content order3.id
     expect(page).to have_content order4.id
 
-    # expect(page).to have_content "Filter Orders By: "
-    click_on "Ordered:"
+    click_link "View Ordered:"
 
     expect(page).to have_content order1.id
     expect(page).to_not have_content order2.id
     expect(page).to_not have_content order3.id
     expect(page).to_not have_content order4.id
 
-    click_on "All Orders"
+    click_link "All Orders"
 
     expect(page).to have_content order1.id
     expect(page).to have_content order2.id
     expect(page).to have_content order3.id
     expect(page).to have_content order4.id
+  end
+
+  scenario "to only see paid items" do
+    admin = create(:user, role: 1)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    order1 = create(:order_with_items, status: 0)
+    order2 = create(:order_with_items, status: 1)
+    order3 = create(:order_with_items, status: 2)
+    order4 = create(:order_with_items, status: 3)
+
+    visit admin_dashboard_path
+
+    expect(page).to have_content order1.id
+    expect(page).to have_content order2.id
+    expect(page).to have_content order3.id
+    expect(page).to have_content order4.id
+
+    click_link "View Paid:"
+
+    expect(page).to have_content order2.id
+    expect(page).to_not have_content order1.id
+    expect(page).to_not have_content order3.id
+    expect(page).to_not have_content order4.id
+  end
+
+  scenario "to only see cancelled items" do
+    admin = create(:user, role: 1)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    order1 = create(:order_with_items, status: 0)
+    order2 = create(:order_with_items, status: 1)
+    order3 = create(:order_with_items, status: 2)
+    order4 = create(:order_with_items, status: 3)
+
+    visit admin_dashboard_path
+
+    expect(page).to have_content order1.id
+    expect(page).to have_content order2.id
+    expect(page).to have_content order3.id
+    expect(page).to have_content order4.id
+
+    click_link "View Cancelled:"
+
+    expect(page).to have_content order3.id
+    expect(page).to_not have_content order1.id
+    expect(page).to_not have_content order2.id
+    expect(page).to_not have_content order4.id
+  end
+
+  scenario "to only see completed items" do
+    admin = create(:user, role: 1)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    order1 = create(:order_with_items, status: 0)
+    order2 = create(:order_with_items, status: 1)
+    order3 = create(:order_with_items, status: 2)
+    order4 = create(:order_with_items, status: 3)
+
+    visit admin_dashboard_path
+
+    expect(page).to have_content order1.id
+    expect(page).to have_content order2.id
+    expect(page).to have_content order3.id
+    expect(page).to have_content order4.id
+
+    click_link "View Completed:"
+
+    expect(page).to have_content order4.id
+    expect(page).to_not have_content order1.id
+    expect(page).to_not have_content order2.id
+    expect(page).to_not have_content order3.id
   end
 end
