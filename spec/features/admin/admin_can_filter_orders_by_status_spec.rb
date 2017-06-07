fb5c5102c35ffeca6ff60a906eebc2ec5d15f3cf
squeemishly <squeemishly@gmail.com>
2017-06-07 16:06:42 -0600

    deleted extra letter from the gemfile, modified test to click_link

--- spec/features/admin/admin_can_filter_orders_by_status_spec.rb
+++ spec/features/admin/admin_can_filter_orders_by_status_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "an admin can filter orders list by status" do
-  scenario "when they are in their dashboard" do
+  scenario "to only see ordered items" do
     admin = create(:user, role: 1)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
     order1 = create(:order_with_items, status: 0)
@@ -16,18 +16,14 @@ RSpec.describe "an admin can filter orders list by status" do
     expect(page).to have_content order3.id
     expect(page).to have_content order4.id
 
-<<<<<<< HEAD
-=======
-    # expect(page).to have_content "Filter Orders By: "
->>>>>>> a56e50706e4815bde3e8ed2b83de50bbd8727793
-    click_on "Ordered:"
+    click_link "View Ordered:"
 
     expect(page).to have_content order1.id
     expect(page).to_not have_content order2.id
     expect(page).to_not have_content order3.id
     expect(page).to_not have_content order4.id
 
-    click_on "All Orders"
+    click_link "All Orders"
 
     expect(page).to have_content order1.id
     expect(page).to have_content order2.id
